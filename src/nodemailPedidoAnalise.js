965bd9e33ddd2edd51e1ad27783c233d97af1e73
Matheus <issidios@gmail.com>
2021-07-11 12:24:49 -0400

    Corrigindo

--- src/nodemailPedidoAnalise.js
+++ src/nodemailPedidoAnalise.js
@@ -20,60 +20,6 @@ module.exports = (email, nome, celular, cidade, valorTotal) => {
         
 Olá Magalhães Engenharia, <br/>
         <b>${nome}</b>, de ${cidade}, solicitou um novo pedido de produtos no valor total de R$ ${valorTotal}. <br/><br/>        Abaixo as informações de contato:<br/>        <li>Email: ${email}</li>        <li>Celular: ${celular}</li>        <br/><br/>        para analisar o pedido mais de perto, acesso este <a href='http://magalhaesengenharia.com/admin/pedido/'>link</a>.
-        </p>
-</td>
-</tr>
-</table></td>
-</tr>
-</table></td>
-</tr>
-</table></td>
-</tr>
-</table>
-<table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
-<tr>
-<td align="center" style="padding:0;Margin:0">
-<table class="es-content-body" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px">
-<tr>
-<td style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px;background-color:#efefef" bgcolor="#efefef" align="left">
-<!--[if mso]><table style="width:560px" cellpadding="0" cellspacing="0"><tr><td style="width:180px" valign="top"><![endif]-->
-<table class="es-left" cellspacing="0" cellpadding="0" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left">
-<tr>
-<td class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;width:180px">
-<table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-<tr>
-<td class="es-infoblock es-m-txt-c" align="left" style="padding:0;Margin:0;padding-top:5px;line-height:14px;font-size:12px;color:#CCCCCC"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px"><a target="_blank" href="https://magalhaesengenharia.com" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#CCCCCC;font-size:12px">View email in your browser</a></p></td>
-</tr>
-</table></td>
-</tr>
-</table>
-<!--[if mso]></td><td style="width:20px"></td><td style="width:360px" valign="top"><![endif]-->
-<table cellspacing="0" cellpadding="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-<tr>
-<td align="left" style="padding:0;Margin:0;width:360px">
-<table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-<tr>
-<td class="es-infoblock es-m-txt-c" align="right" style="padding:0;Margin:0;line-height:0px;font-size:0px;color:#CCCCCC">
-<table class="es-table-not-adapt es-social" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
-<tr>
-<td valign="top" align="center" style="padding:0;Margin:0;padding-right:10px"><a target="_blank" href="https://www.facebook.com/engenhariamagalhaes" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#CCCCCC;font-size:12px"><img title="Facebook" src="https://rvegad.stripocdn.email/content/assets/img/social-icons/logo-colored-bordered/facebook-logo-colored-bordered.png" alt="Fb" width="32" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic"></a></td>
-<td valign="top" align="center" style="padding:0;Margin:0"><a target="_blank" href="https://www.instagram.com/engenhariamagalhaes/" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#CCCCCC;font-size:12px"><img title="Instagram" src="https://rvegad.stripocdn.email/content/assets/img/social-icons/logo-colored-bordered/instagram-logo-colored-bordered.png" alt="Inst" width="32" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic"></a></td>
-</tr>
-</table></td>
-</tr>
-</table></td>
-</tr>
-</table>
-<!--[if mso]></td></tr></table><![endif]--></td>
-</tr>
-</table></td>
-</tr>
-</table></td>
-</tr>
-</table>
-</div>
-</body>
-</html>
         `
     }
 
