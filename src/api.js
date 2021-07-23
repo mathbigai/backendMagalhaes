3d846435878db6597b40bade4dc6f36023d1e5fc
Matheus <issidios@gmail.com>
2021-07-23 10:50:43 -0400

    Testando

--- src/api.js
+++ src/api.js
@@ -131,20 +131,7 @@ app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, respo
       response.status(400).send(`Webhook Error: ${err.message}`);
     }
   
-    // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-        console.log('PaymentIntent was successful!');
-        break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        console.log('PaymentMethod was attached to a Customer!');
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
+    
   
     // Return a response to acknowledge receipt of the event
     response.json({received: true});
