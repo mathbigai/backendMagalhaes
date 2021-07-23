086541586cc51b53d747d8ea851d047b841a3878
Matheus <issidios@gmail.com>
2021-07-23 10:39:55 -0400

    Testando

--- src/api.js
+++ src/api.js
@@ -118,31 +118,7 @@ app.post("/stripe/charge/secret", cors(), async (req, res) => {
     }
 })
 
-app.post('/webhook', (request, response) => {
-    const event = request.body;
 
-    // Handle the event
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-            const paymentIntent = event.data.object;
-            console.log('PaymentIntent was successful!');
-            break;
-        case 'payment_method.attached':
-            const paymentMethod = event.data.object;
-            console.log('PaymentMethod was attached to a Customer!');
-            break;
-        case 'charge.succeeded':
-            const paymentMethod = event.data.object;
-            console.log('PaymentMethod was attached to a Customer!');
-            break;
-        // ... handle other event types
-        default:
-            console.log(`Unhandled event type ${event.type}`);
-    }
-
-    // Return a 200 response to acknowledge receipt of the event
-    response.json({ received: true });
-});
 
 
 const server = http.createServer(app);
